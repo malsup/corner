ecc2c5b5ca376b45a78d2ef19f152febc06518cc
Mike <admin@MacBookPro.local>
2009-07-28 16:33:50 -0400

    Fix date.

--- jquery.corner.js
+++ jquery.corner.js
@@ -1,7 +1,7 @@
 /*!
  * jQuery corner plugin: simple corner rounding
  * Examples and documentation at: http://jquery.malsup.com/corner/
- * version 1.99 (28-JUN-2009)
+ * version 1.99 (28-JUL-2009)
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
